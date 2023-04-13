cda9958c6e1522428b8abb84c3e491859057e97c
Hassanamiri <hassanamiri369@gmail.com>
2023-04-13 13:06:33 +0330

    create postlist page

---
+++ app/posts/page.tsx
@@ -0,0 +1,16 @@
+
+
+export const metadata = {
+    title : "Post Page"
+}
+
+
+const PostList = ()=>{
+    return (
+        <div>
+            Post List Page
+        </div>
+    )
+}
+
+export default PostList;
